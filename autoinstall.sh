ffda2e0dd7e491c58f341588f4c9d06e13940d49
ILYASSE237 <9.12.2006joel@gmail.com>
2026-08-25 21:46:17 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
-REPO_RAW="${REPO_RAW:-https://raw.githubusercontent.com/joeltom-tech/JOELTOM_VPN/main}"
+REPO_RAW="${REPO_RAW:-https://raw.githubusercontent.com/ILYASSE237/JOELTOM_VPN/main}"
 RED='\033[0;31m'; GREEN='\033[0;32m'; BLUE='\033[0;34m'; NC='\033[0m'
 [ "$EUID" -eq 0 ] || { echo -e "${RED}❌ Exécutez ce script en root.${NC}"; exit 1; }
 command -v curl >/dev/null 2>&1 || { apt-get update -y && apt-get install -y curl ca-certificates; }
